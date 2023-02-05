cc80696be39a382dca2548c5769a5d35be5149fe
elprofe-DR-hp <edjomajibusiness@gmail.com>
2023-02-05 16:06:09 -0400

    updated the getAllTks thing on browser-app.js cuz it was not working since I had a differeent name for it on the controller.

--- public/browser-app.js
+++ public/browser-app.js
@@ -8,14 +8,14 @@ const showTasks = async () => {
   loadingDOM.style.visibility = 'visible'
   try {
     const {
-      data: { tasks },
+      data: { getAllTks },
     } = await axios.get('/api/v1/tasks')
-    if (tasks.length < 1) {
+    if (getAllTks.length < 1) {
       tasksDOM.innerHTML = '<h5 class="empty-list">No tasks in your list</h5>'
       loadingDOM.style.visibility = 'hidden'
       return
     }
-    const allTasks = tasks
+    const allTasks = getAllTks
       .map((task) => {
         const { completed, _id: taskID, name } = task
         return `<div class="single-task ${completed && 'task-completed'}">
